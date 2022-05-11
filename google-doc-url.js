1d272533bba18421fc8aa262eb03671f05e91e2c
ccrivietnam <97395731+ccrivietnam@users.noreply.github.com>
2022-05-11 20:46:09 +0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1oXsdJFkRTHXGpV5bsHJ0Ls44JSAcCsb-18XH3K_jo-4/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Pjdpy-ez4_3ndvoRZ3f4A5_KPOjAm2krGfauyxLwWo0/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
